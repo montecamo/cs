d3d43e215bdd68c8ee5a55984ed46f058524041c
montecamo <merrychristek@gmail.com>
2022-05-28 11:10:05 +0300

    craftinginterpreters: lox 8th chapter

--- craftinginterpreters/lox/Interpreter.java
+++ craftinginterpreters/lox/Interpreter.java
@@ -1,6 +1,10 @@
 package com.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
+import java.util.List;
+
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+  private Environment environment = new Environment();
+
   @Override
   public Object visitLiteralExpr(Expr.Literal expr) {
     return expr.value;
@@ -56,7 +60,6 @@ class Interpreter implements Expr.Visitor<Object> {
             "Operands must be two numbers or two strings."
         );
 
-        break;
       case GREATER:
         checkNumberOperands(expr.operator, left, right);
         return (double)left > (double)right;
@@ -75,6 +78,42 @@ class Interpreter implements Expr.Visitor<Object> {
 
     // Unreachable.
     return null;
+  }
+
+  @Override
+  public Void visitExpressionStmt(Stmt.Expression stmt) {
+    evaluate(stmt.expression);
+    return null;
+  }
+
+  @Override
+  public Void visitPrintStmt(Stmt.Print stmt) {
+    Object value = evaluate(stmt.expression);
+    System.out.println(stringify(value));
+    return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, value);
+    return value;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
   }
 
   private Object evaluate(Expr expr) {
@@ -119,12 +158,36 @@ class Interpreter implements Expr.Visitor<Object> {
     return object.toString();
   }
 
-  void interpret(Expr expression) { 
+  void interpret(List<Stmt> statements) {
     try {
-      Object value = evaluate(expression);
-      System.out.println(stringify(value));
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
     } catch (RuntimeError error) {
       Lox.runtimeError(error);
+    }
+  }
+
+  private void execute(Stmt stmt) {
+    stmt.accept(this);
+  }
+
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
+  void executeBlock(List<Stmt> statements, Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
+
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
     }
   }
 }
