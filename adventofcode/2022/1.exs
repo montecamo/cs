495713153bcf6180dd230db8d14423d26f80dee4
montecamo <merrychristek@gmail.com>
2023-10-28 19:09:29 +0300

    adventofcode: 2022 1.2

--- adventofcode/2022/1.exs
+++ adventofcode/2022/1.exs
@@ -1,17 +1,14 @@
-output =
-  File.stream!("1.input")
-  |> Stream.map(fn x ->
-    case x do
-      "\n" -> x
-      str -> String.replace(str, "\n", "") |> String.to_integer()
-    end
-  end)
-  |> Enum.reduce(%{current: 0, max: 0}, fn calorie, %{current: current, max: max} ->
-    case calorie do
-      "\n" when current > max -> %{current: 0, max: current}
-      "\n" -> %{current: 0, max: max}
-      _ -> %{current: current + calorie, max: max}
-    end
-  end)
-
-IO.puts(output.max)
+File.stream!("1.input")
+|> Stream.map(&String.replace(&1, "\n", ""))
+|> Stream.map(fn x ->
+  case x do
+    "" -> 0
+    str -> String.to_integer(str)
+  end
+end)
+|> Stream.chunk_by(&(&1 == 0))
+|> Stream.map(&Enum.sum(&1))
+|> Enum.sort()
+|> Enum.take(-3)
+|> Enum.sum()
+|> IO.puts()
