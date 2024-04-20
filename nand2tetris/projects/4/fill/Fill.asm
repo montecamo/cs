5d2ae643ec6dbc8234bd4c21292356151b6c5ad7
Timur Buslov <merrychristek@gmail.com>
2024-04-20 18:49:17 +0300

    nand2tetris: project4 done

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -8,4 +8,48 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+  @24576 // last screen address
+  D=A
+  @screenlast
+  M=D
+
+(FILLSCREEN)
+  @SCREEN
+  D=A
+  @screencurrent
+  M=D
+
+(LOOP)
+  // for loop
+  @screencurrent
+  D=M
+  @screenlast
+  D=D-M
+  @FILLSCREEN
+  D;JEQ
+
+  // check key pressed 
+  @KBD
+  D=M
+  @WHITE
+  D;JEQ
+
+(BLACK)
+  @screencurrent
+  A=M
+  M=-1
+
+  @ENDIF
+  0;JMP
+(WHITE)
+  @screencurrent
+  A=M
+  M=0
+(ENDIF)
+
+  // set next pixel address
+  @screencurrent
+  M=M+1
+
+  @LOOP
+  0;JMP
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -6,5 +6,28 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
+  @R2
+  M=0
 
-//// Replace this comment with your code.
+(LOOP)
+  // for loop
+  @R1
+  D=M
+  @END
+  D;JEQ
+
+  // addition
+  @R0
+  D=M
+  @R2
+  M=M+D
+
+  // R1--
+  @R1
+  M=M-1
+
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
